0a3a21472d4886f51e5dd299f43a2c7f52ccb032
sacha-smz <sacha.zacaropoulos@gmail.com>
2020-10-02 18:40:30 +0200

    chore: serve card background images

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -8,6 +9,8 @@ const router = require("./app/routes");
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_APP_URL }));
+
+app.use(express.static(path.resolve("public")));
 
 app.use(express.urlencoded({ extended: true }), express.json());
 
